a0d070481cae1b701f2e62b14046d15fea27d2e1
Shahjamal Biswas <shahjamal570@gmail.com>
2022-11-19 13:50:59 +0530

    Venor profile form + validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from tkinter import Widget
 from django import forms
 from .models import User, UserProfile
+from .validators import allow_only_images_validator
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -21,6 +22,19 @@ class UserForm(forms.ModelForm):
             )
 
 class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Start Typing ...','required':'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    
+    #1st Method for making lat and long fields readonly:
+    #latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    #longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
     class Meta:
         model = UserProfile
-        fields = ['profile_picture','cover_photo','address_line_1','address_line_2','country','state','pin_code','latitude','longitude']
+        fields = ['profile_picture','cover_photo','address','country','city','state','pin_code','latitude','longitude']
+    #2nd Method for making lat and long fields readonly:
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
