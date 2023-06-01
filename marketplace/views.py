acb867196360acadc45437c69c304f5d5f93f2fa
Shahjamal Biswas <shahjamal570@gmail.com>
2023-06-01 18:22:06 +0530

    Cart Funstion with AJAX

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from menu.models import Category, FoodItem
 from vendor.models import Vendor
 from django.db.models import Prefetch
+from .models import Cart
+from .context_processors import get_cart_counter
 
 # Create your views here.
 
@@ -23,9 +26,67 @@ def vendor_detail(request, vendor_slug):
          queryset=FoodItem.objects.filter(is_available=True)
       )
    )
+
+   if request.user.is_authenticated:
+      cart_items = Cart.objects.filter(user=request.user)
+   else:
+      cart_items = None
+
    context = {
       'vendor' : vendor ,
       'categories': categories,
+      'cart_items': cart_items,
    }
 
    return render(request, 'maketplace/vendor_detail.html', context)
+
+def add_to_cart(request, food_id):
+   if request.user.is_authenticated:
+      if request.headers.get('x-requested-with') == 'XMLHttpRequest': # Check if it is ajax request
+         # Check if the food item exists
+         try:
+            fooditem = FoodItem.objects.get(id=food_id)
+            # Check if the user has already added the item to the cart
+            try:
+               chkCart= Cart.objects.get(user=request.user, fooditem=fooditem)
+               # Increase cart quantity
+               chkCart.quantity += 1
+               chkCart.save()
+               return JsonResponse({'satus':'success', 'message': 'Increased cart quantity', 'cart_counter': get_cart_counter(request),'qty': chkCart.quantity})
+            except:
+               chkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity = 1)
+               return JsonResponse({'satus':'success', 'message': 'Added the item to the cart', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity})
+         except:
+            return JsonResponse({'status': 'Failed', 'message':'This Item does not exist'})
+      else:
+         return JsonResponse({'status': 'Failed', 'message':'Invalid Request!'})
+   else:
+      return JsonResponse({'status': 'login_required', 'message':'Please login to continue'}) # We don't want to reload the page hence simply reuring te http json response
+
+
+
+def decrease_cart(request, food_id):
+   if request.user.is_authenticated:
+      if request.headers.get('x-requested-with') == 'XMLHttpRequest': # Check if it is ajax request
+         # Check if the food item exists
+         try:
+            fooditem = FoodItem.objects.get(id=food_id)
+            # Check if the user has already added the item to the cart
+            try:
+               chkCart= Cart.objects.get(user=request.user, fooditem=fooditem)
+               if chkCart.quantity > 1:
+                  # Decrease cart quantity
+                  chkCart.quantity -= 1
+                  chkCart.save()
+               else:
+                  chkCart.delete()
+                  chkCart.quantity = 0
+               return JsonResponse({'satus':'success',  'cart_counter': get_cart_counter(request),'qty': chkCart.quantity})
+            except:
+               return JsonResponse({'satus':'Failed', 'message': 'You do not have this item in your cart!'})
+         except:
+            return JsonResponse({'status': 'Failed', 'message':'This Item does not exist'})
+      else:
+         return JsonResponse({'status': 'Failed', 'message':'Invalid Request!'})
+   else:
+      return JsonResponse({'status': 'login_required', 'message':'Please login to continue'})
---
+++ static/js/custom.js
@@ -0,0 +1,34 @@
+let autocomplete;
+
+function initAutoComplete(){
+autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById('id_address'),
+    {
+        types: ['geocode', 'establishment'],
+        //default in this app is "IN" - add your country code
+        componentRestrictions: {'country': ['in']},
+    })
+// function to specify what should happen when the prediction is clicked
+autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+function onPlaceChanged (){
+    var place = autocomplete.getPlace();
+
+    // User did not select the prediction. Reset the input field or alert()
+    if (!place.geometry){
+        document.getElementById('id_address').placeholder = "Start typing...";
+    }
+    else{
+        console.log('place name=>', place.name)
+    }
+    // get the address components and assign them to the fields
+}
+
+$(document).ready(function(){
+    $(document).on('click', '.add_to_cart', function(e){
+        e.preventDefault();
+        food_id = $(this).attr('data-id');
+        alert(food_id);
+    })
+});
